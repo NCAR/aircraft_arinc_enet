06a8dfe1d577aec91406dbc647e27b62faa6c684
topher800 <cjw@ucar.edu>
2019-09-16 21:32:04 +0000

    Add signal handling.

--- rdAltaUDP/main.cc
+++ rdAltaUDP/main.cc
@@ -1,13 +1,28 @@
 #include <QApplication>
 
+#include <csignal>
+
 #include "rd_udp.h"
+
+RdUDP *rdr = 0;
+
+void sighandler(int s)
+{
+  delete rdr;
+  exit(0);
+}
+
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv, false);
 
-  RdUDP	rdr;
+  rdr = new RdUDP();
+
+  signal(SIGINT, sighandler);
+  signal(SIGFPE, sighandler);
+  signal(SIGTERM, sighandler);
+  signal(SIGSEGV, sighandler);
 
   app.exec();
- 
 }
